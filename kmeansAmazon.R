1312aeae51b0bf6a5cda3fb3dd3808d7d29b52ed
fredyvel <fvelasquezq3@gmail.com>
2020-05-24 01:10:59 -0600

    kmeans realizados

--- kmeansAmazon.R
+++ kmeansAmazon.R
@@ -9,3 +9,13 @@ for(i in 2:20) wss[i]<-sum(kmeans(data,centers = i)$withinss)
 plot(1:20,wss,type='b',xlab="Numero de Cluster",ylab="withniss group")
 
 mycluster<-kmeans(data,8,nstart = 5, iter.max = 30)
+
+install.packages("fmsb")
+library(fmsb)
+par(mfrow=c(2,4))
+plotCentros<-function(centro){
+dat<-as.data.frame(t(mycluster$centers[centro,]))
+dat<-rbind(rep(5,10),rep(-1.5,10),dat)
+radarchart(dat)
+}
+sapply(c(1:8),plotCentros)
